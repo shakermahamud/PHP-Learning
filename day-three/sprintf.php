516cd3363cf709d5d82fb44f27437ddc81e527e3
shakermahamud <admin@shakermahamud.com>
2022-12-30 23:03:38 +0600

    sprintf add

---
+++ day-three/sprintf.php
@@ -0,0 +1,16 @@
+<?php
+
+$fName= 'Shaker';
+$mName= 'Ullah';
+$lName= 'Mahamud';
+
+$myFullName = sprintf('My name is %s %s %s',$fName,$mName,$lName);
+echo $myFullName;
+echo "\n";
+echo strtoupper($myFullName);
+echo "\n";
+echo strtolower($myFullName);
+echo "\n";
+
+
+
