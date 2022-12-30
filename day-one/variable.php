ec3d1958b13b387f84423af13e5b2c3edc7007ad
shakermahamud <admin@shakermahamud.com>
2022-12-30 21:01:45 +0600

    variable some code add

---
+++ day-one/variable.php
@@ -0,0 +1,15 @@
+<?php
+
+$firstName = 'Shaker';
+$lastName = 'Mahamud';
+$myAge= 23;
+$myNewAge = 'myAge';
+$myPorpession = 'Full Stack MARN And PHP Leraval Developer';
+
+echo "My name is {$firstName} {$lastName}\n";
+echo "My name is ".$myAge."\n";
+echo "My age is {$myAge}\n";
+echo "My propession is {$myPorpession}\n";
+echo $myNewAge."\n";
+echo $$myNewAge;
+
