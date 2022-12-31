6240c229b565a54ab3cef0f5950e62bf8eac2206
shakermahamud <admin@shakermahamud.com>
2022-12-31 21:20:58 +0600

    fac

---
+++ day-four/factorial.php
@@ -0,0 +1,8 @@
+<?php
+$n = 3;
+
+for($i=$n,$fac=1;$i>1;$i--){
+    $fac +=$i;
+}
+
+printf('%d to %d',$n,$fac);
---
+++ day-four/multipleLoop.php
@@ -0,0 +1,7 @@
+<?php
+
+for($i=10,$j=1;$i>0;$i--,$j++){
+    echo $i." : ".$j;
+    echo PHP_EOL;
+}
+
